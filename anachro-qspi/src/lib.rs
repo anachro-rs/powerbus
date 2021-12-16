e351005ebeb93d8f661893b827f6518cfd93baf1
James Munns <james@onevariable.com>
2021-12-16 13:24:48 +0100

    Add anachro qspi

---
+++ anachro-qspi/src/lib.rs
@@ -0,0 +1,233 @@
+//! # Anachro QSPI
+//!
+//! ## Notes
+//!
+//! This is not a general purpose library!
+//!
+//! Chip: W25Q128JVSIQ
+//! note: This chip defaults to quad mode enabled!
+//!
+//! Manufacturer ID: 0xEF
+//! Device ID: 0x17_4018
+//!
+//! * 256-byte pages (writeable) - 65536x
+//! * 4KiB sectors (eraseable) - 4096x
+//! * 32KiB Small-Blocks (eraseable) - 512x
+//! * 64KiB Large-Blocks (eraseable) - 256x
+//! * 16MiB Whole Chip (eraseable)
+//!
+//! What do I need the flash chip for?
+//!
+//! * Storing firmware images - Up to 1MiB each
+//! * Keeping a rolling buffer of log messages?
+//!     * Device has a lifetime of 100K prog/erase cycles
+//!     * At 8Mib for logs, how long would it take to generate 781.25 GiB of log data?
+//!
+//! Okay, let's plan out a memory map
+//!
+//! MB - ADDR Range
+//! ===========================================
+//! 00 - 0x000000 - 0x0FFFFF: Config
+//! 01 - 0x100000 - 0x1FFFFF:
+//! 02 - 0x200000 - 0x2FFFFF:
+//! 03 - 0x300000 - 0x3FFFFF:
+//! 04 - 0x400000 - 0x4FFFFF: Firmware Slot 1
+//! 05 - 0x500000 - 0x5FFFFF: Firmware Slot 2
+//! 06 - 0x600000 - 0x6FFFFF:
+//! 07 - 0x700000 - 0x7FFFFF:
+//! 08 - 0x800000 - 0x8FFFFF: A4 Slot 1
+//! 09 - 0x900000 - 0x9FFFFF: A4 Slot 2
+//! 10 - 0xA00000 - 0xAFFFFF:
+//! 11 - 0xB00000 - 0xBFFFFF:
+//! 12 - 0xC00000 - 0xCFFFFF:
+//! 13 - 0xD00000 - 0xDFFFFF:
+//! 14 - 0xE00000 - 0xEFFFFF:
+//! 15 - 0xF00000 - 0xFFFFFF:
+
+#![cfg_attr(not(test), no_std)]
+
+use core::{sync::atomic::Ordering, task::Poll};
+
+use byte_slab::ManagedArcSlab;
+
+pub struct FlashChunk<const CT: usize, const SZ: usize> {
+    addr: usize,
+    data: ManagedArcSlab<'static, CT, SZ>,
+}
+
+use cassette::futures::poll_fn;
+use nrf52840_hal::{
+    gpio::{Disconnected, Pin, Port},
+    pac::{P0, P1, QSPI},
+};
+
+pub use nrf52840_hal::pac::qspi::erase::len::LEN_A as EraseLength;
+
+pub struct QspiPins {
+    pub qspi_copi_io0: Pin<Disconnected>,
+    pub qspi_cipo_io1: Pin<Disconnected>,
+    pub qspi_io2: Pin<Disconnected>, // Also "WPn"
+    pub qspi_io3: Pin<Disconnected>, // Also "HOLDn"
+    pub qspi_csn: Pin<Disconnected>,
+    pub qspi_sck: Pin<Disconnected>,
+}
+
+pub struct Qspi {
+    _pins: QspiPins,
+    periph: QSPI,
+}
+
+pub enum Error {
+    /// Address was not aligned properly
+    Alignment,
+}
+
+impl Qspi {
+    pub fn new(periph: QSPI, pins: QspiPins) -> Self {
+        let pinarr = &[
+            &pins.qspi_copi_io0,
+            &pins.qspi_cipo_io1,
+            &pins.qspi_io2,
+            &pins.qspi_io3,
+            &pins.qspi_csn,
+            &pins.qspi_sck,
+        ];
+
+        for pin in pinarr {
+            let port = match pin.port() {
+                Port::Port0 => unsafe { &*P0::ptr() },
+                Port::Port1 => unsafe { &*P1::ptr() },
+            };
+
+            port.pin_cnf[pin.pin() as usize].modify(|_r, w| w.drive().h0h1());
+        }
+
+        unsafe {
+            periph.psel.io0.write(|w| {
+                w.bits(pins.qspi_copi_io0.psel_bits());
+                w.connect().connected()
+            });
+            periph.psel.io1.write(|w| {
+                w.bits(pins.qspi_cipo_io1.psel_bits());
+                w.connect().connected()
+            });
+            periph.psel.io2.write(|w| {
+                w.bits(pins.qspi_io2.psel_bits());
+                w.connect().connected()
+            });
+            periph.psel.io3.write(|w| {
+                w.bits(pins.qspi_io3.psel_bits());
+                w.connect().connected()
+            });
+            periph.psel.csn.write(|w| {
+                w.bits(pins.qspi_csn.psel_bits());
+                w.connect().connected()
+            });
+            periph.psel.sck.write(|w| {
+                w.bits(pins.qspi_sck.psel_bits());
+                w.connect().connected()
+            });
+        }
+
+        periph.ifconfig0.modify(|_r, w| {
+            w.readoc().read4io();
+            w.writeoc().pp4o();
+            w.addrmode()._24bit();
+            w.dpmenable().disable();
+            w.ppsize()._256bytes();
+            w
+        });
+        periph.ifconfig1.modify(|_r, w| {
+            // One 16-mhz cycle delay. As far as I can tell, we don't
+            // even need this?
+            unsafe { w.sckdelay().bits(1) };
+            w.dpmen().exit();
+            w.spimode().mode0();
+            // SPI freqency of 32mhz
+            unsafe { w.sckfreq().bits(0) };
+            w
+        });
+
+        // Enable QSPI peripheral
+        periph.enable.modify(|_r, w| w.enable().enabled());
+
+        // Clear the "is ready" flag
+        periph.events_ready.reset();
+
+        // Activate the flash device
+        periph
+            .tasks_activate
+            .write(|w| w.tasks_activate().set_bit());
+
+        // Wait for the ready flag
+        while periph.events_ready.read().events_ready().bit_is_clear() {}
+
+        // Make sure no reads happen BEFORE the QSPI is enabled
+        core::sync::atomic::compiler_fence(Ordering::SeqCst);
+
+        Self {
+            _pins: pins,
+            periph,
+        }
+    }
+
+    pub fn read_slice(&self, flash_addr: usize, len: usize) -> &[u8] {
+        assert!(flash_addr < (16 * 1024 * 1024));
+        assert!((flash_addr + len) <= (16 * 1024 * 1024));
+        unsafe {
+            core::slice::from_raw_parts(
+                (flash_addr + 0x12000000) as *const u8,
+                len
+            )
+        }
+    }
+
+    pub async fn write<const CT: usize, const SZ: usize>(&mut self, data: FlashChunk<CT, SZ>) -> Result<(), Error> {
+        if data.data.len() != 256 {
+            // For now, only handle full page writes
+            return Err(Error::Alignment);
+        }
+        if data.addr & 0xFF != 0 {
+            return Err(Error::Alignment);
+        }
+
+        self.periph.write.dst.write(|w| unsafe { w.bits(data.addr as u32)});
+        self.periph.write.src.write(|w| unsafe { w.bits(data.data.as_ptr() as u32)});
+        self.periph.write.cnt.write(|w| unsafe { w.bits(256)});
+        self.periph.events_ready.reset();
+        core::sync::atomic::compiler_fence(Ordering::SeqCst);
+        self.periph.tasks_writestart.write(|w| w.tasks_writestart().set_bit());
+
+        self.wait_done().await;
+
+        Ok(())
+    }
+
+    pub async fn erase(&mut self, start: usize, len: EraseLength) -> Result<(), Error> {
+        // Ensure alignment to page size
+        match len {
+            EraseLength::_4KB if start & 0xFFF != 0 => return Err(Error::Alignment),
+            EraseLength::_64KB if start & 0xFFFF != 0 => return Err(Error::Alignment),
+            EraseLength::ALL if start != 0 => return Err(Error::Alignment),
+            _ => {}
+        }
+
+        self.periph.erase.ptr.write(|w| unsafe { w.bits(start as u32) });
+        self.periph.erase.len.write(|w| w.len().variant(len) );
+        self.periph.events_ready.reset();
+        self.periph.tasks_erasestart.write(|w| w.tasks_erasestart().set_bit());
+
+        Ok(self.wait_done().await)
+    }
+
+    pub async fn wait_done(&self) {
+        poll_fn(|_| {
+            if self.periph.events_ready.read().events_ready().bit_is_clear() {
+                Poll::Pending
+            } else {
+                Poll::Ready(())
+            }
+        }).await
+    }
+}
+
